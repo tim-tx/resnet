2852db3e5aa2cbb1b2939355f705b57c5b35f4e5
Tim <jonests@seas.upenn.edu>
2016-03-04 12:51:47 -0500

    Offload common functions to separate file, unify the loading and generating functions via command line arguments.

---
+++ resnet.py
@@ -0,0 +1,15 @@
+import scipy.sparse
+
+def csr_row_set_nz_to_val(csr, row, value=0):
+    """Set all nonzero elements (elements currently in the sparsity pattern)
+    to the given value. Useful to set to 0 mostly.
+    """
+    if not isinstance(csr, scipy.sparse.csr_matrix):
+        raise ValueError('Matrix given must be of CSR format.')
+    csr.data[csr.indptr[row]:csr.indptr[row+1]] = value
+
+def csr_rows_set_nz_to_val(csr, rows, value=0):
+    for row in rows:
+        csr_row_set_nz_to_val(csr, row)
+    if value == 0:
+        csr.eliminate_zeros()
